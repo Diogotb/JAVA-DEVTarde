bba950343a7d17868b8cda2153aec505b891e6f8
Diogo Takamori Barbosa <42560225+Diogotb@users.noreply.github.com>
2023-05-15 18:31:02 +0000

    15/05

---
+++ POOIntroducao/App.java
@@ -0,0 +1,34 @@
+package POOIntroducao;
+
+/**
+ * App
+ */
+public class App {
+
+    public static void main(String[] args) {
+        // criar 3 objetos ResgistraAluno
+        RegistraAluno ana = new RegistraAluno();
+        RegistraAluno beto = new RegistraAluno();
+        RegistraAluno carlos = new RegistraAluno();
+        
+        //altera as informações com o set
+        ana.setNome("Ana Machado");
+        ana.setEndereco("Rua das Flores, 123");
+        ana.setIdade(29);
+        ana.setNotaCiencias(9.5);
+        ana.setNotaMatematica(8.3);
+        ana.setNotaPortugues(7.5);
+
+        beto.setNome("Roberto da Silva");
+        carlos.setNome("Carlos Alberto");
+        
+        //printa com as informações com o get(acessa as informações)
+        System.out.println("Nome:"+ana.getNome());
+        System.out.println(ana.getEndereco());
+        System.out.println(ana.getIdade());
+        System.out.println(ana.getMedia());
+        System.out.println("=================");
+        System.out.println("Contador: "
+                + RegistraAluno.getQuantidadeAlunos());
+    }
+}
