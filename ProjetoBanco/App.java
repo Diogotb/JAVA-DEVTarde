2b19a9758399b9252c89b73e8df9004d9aef5628
Diogotb <digogo@gmail.com>
2023-06-12 17:03:20 -0300

    12/06

--- ProjetoBanco/App.java
+++ ProjetoBanco/App.java
@@ -12,6 +12,7 @@ public class App {
     ContaPJ contasPj[] = new ContaPJ[10];
     boolean agenciaAberta = true;
     int cont = 0;
+    int contaAtual=0;
     while(agenciaAberta){
         int acao1 = Integer.parseInt(JOptionPane.showInputDialog("Informe a ação desejada:"
                         +"\n 1 - Criar Conta;"
@@ -19,15 +20,32 @@ public class App {
         if(acao1==1){
             //criar conta(vou fazer primeiro da PF)
             contasPf[cont] = new ContaPF();//obj criado
-            //atribuir infos
+            //atribuir infos da conta criada
             contasPf[cont].setNome(JOptionPane.showInputDialog("Informe o Nome:"));
             contasPf[cont].setNumeroConta(1000+cont+1);
             contasPf[cont].setSaldo(0);
             contasPf[cont].setnCpf(JOptionPane.showInputDialog("Informe o CPF:"));
-
+            cont++;
 
         } else if (acao1 == 2) {
-            //acessar conta
+            //acessar conta - percorrer o vetor criado eprocurar um valor ou nº da conta ou nome
+            int nContaBuscada = Integer.parseInt(JOptionPane.showInputDialog("Digite o nº da Conta"));
+            for (int i = 0; i < contasPf.length; i++) {
+                if (nContaBuscada == contasPf[i].getNumeroConta()) {
+                    contaAtual = i;
+                    JOptionPane.showMessageDialog(null, "Conta Encontrada");
+                }
+            }
+            boolean acessar = true;
+            //
+            while(acessar){
+                //opções dos métodos da conta PF
+                int opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe a Opção Desejada"));
+                if(opcao ==1){
+                    contasPf[contaAtual].getSaldo();
+                }
+
+            }
 
         } else{
             JOptionPane.showMessageDialog(null, "Digite uma ação válida");
