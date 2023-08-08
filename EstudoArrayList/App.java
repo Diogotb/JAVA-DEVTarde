193fb1e72dc5cfaa5e3e6a26ae936fad342c04e0
Diogotb <digogo@gmail.com>
2023-08-08 15:18:58 -0300

    08/08

--- EstudoArrayList/App.java
+++ EstudoArrayList/App.java
@@ -3,6 +3,11 @@ package EstudoArrayList;
 public class App {
     public static void main(String[] args) {
         ExemploArrayList teste = new ExemploArrayList();
+        teste.add(1);
         teste.add(2);
+        teste.add(3);
+        teste.add(4);
+        teste.add(5);
+        teste.imprimir();
     }
 }
