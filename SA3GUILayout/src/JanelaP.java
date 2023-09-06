59b4a230cacd92628c2b67fa741b0cc9c6e11c7e
Diogotb <digogo@gmail.com>
2023-09-06 18:43:43 -0300

    06/09

---
+++ SA3GUILayout/src/JanelaP.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class JanelaP extends JFrame{
+    public JanelaP() {
+        super("Projeto");
+        //add JtabbedPane a janela Principal
+        this.add(new JanelaAbas());
+        //set Frame
+        this.pack();
+        this.setDefaultCloseOperation(2);
+        this.setVisible(true);
+    }
+    
+}
